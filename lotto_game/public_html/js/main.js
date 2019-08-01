05ed20e3b0f1c8bf5c67b74bc4e9d8e61b2af248
Zivanic <zivanic23@gmail.com>
2019-08-01 02:49:53 +0200

    finished draw, winning tickets, styles for game

--- lotto_game/public_html/js/main.js
+++ lotto_game/public_html/js/main.js
@@ -6,13 +6,15 @@ for (let i = 1; i <= 30; i++) {
 let selectedNumbers = [];
 let ticketNum = 1;
 let tickets = {};
-
+const numToDrow = 12;
+let balance = 1000;
 //------------------------------
 
 document.addEventListener("DOMContentLoaded", function () {
     availableNumbers();
     let addButtton = document.querySelector('#addTicket');
     addButtton.onclick = addTicket;
+    document.querySelector('#balance').innerHTML += balance;
 });
 
 
@@ -69,35 +71,113 @@ function addTicket() {
 function addCompletedTicket(ticket) {//adding comleted ticket
     if (tickets.hasOwnProperty('ticket_' + ticket)) {//check if ticket is added
         let played = document.querySelector('.selected-num-wrapper');
-        let ticketTemplate = '<div ticket-id=ticket_'+ticket+' class="playedTicket"></div>';
+        let ticketTemplate = '<div ticket-id=ticket_' + ticket + ' class="playedTicket"></div>';
         let playedNum = tickets['ticket_' + ticket];
         played.innerHTML += ticketTemplate;
-        let ticketWrapper = document.querySelector('[ticket-id=ticket_'+ticket+']');
-        console.log(ticketWrapper)
-        
+        let ticketWrapper = document.querySelector('[ticket-id=ticket_' + ticket + ']');
+
         playedNum.forEach(num => {
-            console.log('fisrt')
-            ticketWrapper.innerHtml += '<p>'+num+'</p>';
-            console.log(ticketWrapper)
+            ticketWrapper.innerHTML += '<span class="ball">' + num + '</span>';
+
         });
-        console.log('second')
-        
-        
-        
-        
-        
-        
     }
 }
 
 function checkForStart() {
     if (ticketNum == 5) {
         //all ticket are fill and game can start
-        console.log('start game');
+        let addButtton = document.querySelector('#addTicket');
+        addButtton.onclick = startGame;
+        addButtton.textContent = "ZAPOCNI IGRU";
+        addButtton.classList.add('play');
     }
 }
+function loop(callback, interval) {
+    counter = setTimeout(callback, interval);
+}
+
+function startGame() {
+    //Game start
+    let addButtton = document.querySelector('#addTicket');
+    addButtton.textContent = "IZVLACENJE U TOKU...";
+    addButtton.disabled = true;
+    let resultNum = [...allNumbers];
+    let winNum = [];
+    for (let i = 0; i < numToDrow; i++) {//set 12 random numbers from 1-30
+        let randPosition = Math.floor(Math.random() * resultNum.length);
+        let rand = resultNum[randPosition];
+        winNum.push(rand);
+        resultNum.splice(randPosition, 1);
+    }
+
+    let roundNum = 0;
+    (function loopGame() {//looping game until last number
+
+        if (roundNum >= winNum.length) {//check round
+            //Check for win ticket and restart round
+            checkTickets(winNum);
+            return;
+        } else {
+            loop(loopGame, 500);
+            document.querySelector('.draw-wrapper').innerHTML += '<span class="winNum">' + winNum[roundNum] + '</span>';
+            let numbers = document.querySelectorAll('.ball');
+            numbers.forEach(num => {
+                if (num.innerText == winNum[roundNum]) {
+                    num.classList.add('active')
+                }
+            })
+            roundNum++;
+        }
+
+    })();
+}
+
+function checkTickets(winArr) {
+    let ticketArr = Object.entries(tickets)
+    ticketArr.forEach(ticket => {
+        let win = arrayContainsArray(winArr, ticket[1]);
+        let winTicket = document.querySelector('[ticket-id="' + ticket[0] + '"]');
+        if (win == true) {//check for win tickets
+            winTicket.classList.add('win');
+        } else {
+            winTicket.classList.add('lost');
+        }
+
+    })
+    calculateWin();
+
+    setTimeout(function () { restartGame(); }, 3000);//wait 3sec before set game to new round
+
+}
+
+function arrayContainsArray(superset, subset) {
+    if (0 === subset.length) {
+        return false;
+    }
+    return subset.every(function (value) {
+        return (superset.indexOf(value) >= 0);
+    });
+}
+
+function calculateWin() {
+
+}
+
+function restartGame() {
+    //set game to start stage
+    let addButtton = document.querySelector('#addTicket');
+    addButtton.textContent = "DODAJ TIKET";
+    addButtton.disabled = false;
+    addButtton.onclick = addTicket;
+
+    ticketNum = 1;
+    tickets = {};
+
+    document.querySelector('.draw-wrapper').innerHTML = '';
+    document.querySelector('.selected-num-wrapper').innerHTML = '<h3>Tiketi:</h3>';
 
 
+}
 function trowError(err) {
     console.log(err);
 }
